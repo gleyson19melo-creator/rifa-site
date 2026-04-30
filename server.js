49875e34c1148611d445110bdfe49b2978274f06
gleyson19melo <gleyson19melo@gmail.com>
2026-04-30 11:52:57 -0300

    configura firebase no render

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Firebase
-const serviceAccount = require('./firebase.json');
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require('./firebase.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
